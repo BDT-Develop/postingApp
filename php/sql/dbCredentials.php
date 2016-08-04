847263f755f40471ab23e8212b1c7624c64ba125
Rosendo Galindo <rosendo_galindo@hotmail.com>
2016-08-04 13:32:43 -0500

    google cloud testing and credentials

--- php/sql/dbCredentials.php
+++ php/sql/dbCredentials.php
@@ -1,5 +1,13 @@
 <?php
+/*Localhost credentials
 define("NOMBRE_HOST","localhost");
 define("BASE_DE_DATOS","testOne");
 define("USUARIO","root");
 define("CONTRASENA","mkt2016");
+*/
+
+/*Google Credentials*/
+define("NOMBRE_HOST","107.178.209.115");
+define("BASE_DE_DATOS","testOne");
+define("USUARIO","root");
+define("CONTRASENA","bdtsystems");
