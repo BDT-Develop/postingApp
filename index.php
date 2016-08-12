d1983c381c4656277d5b448d699a19e0fb002fe0
ludoliva <somacruz_vampire@hotmail.com>
2016-08-12 00:08:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include("php/com/header.php");
   <h1>STI y BDT Solutions</h1>
    <i class="fa fa-sitemap fa-4x" aria-hidden="true"></i> <i class="fa fa-wrench fa-4x" aria-hidden="true"> | </i> <i class="fa fa-laptop fa-4x" aria-hidden="true"></i> <i class="fa fa-code fa-4x" aria-hidden="true"></i>
   <p>
-    En esta pagina podras ingresar un post con el motivo de visita a tu negocio.
+    En esta pagina podras ingresar un post con el motivo de visita a tu negocio...
   </p>
 
 </div><!--container-->
